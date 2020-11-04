bc1ed98f0918e4fa5cb1a820b7b52a2963f952be
Muhammad Aman <maman0022@outlook.com>
2020-11-03 21:17:17 -0600

    refactored to use hosted database

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://postgres:password@localhost/noteful',
-  API_TOKEN: process.env.API_TOKEN || 'e483162e-8224-4626-8777-1e574d35eede'
+  DB_URL: process.env.DATABASE_URL || '',
+  API_TOKEN: process.env.API_TOKEN || ''
 }
